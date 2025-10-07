068d5e68a1ed3927026de8619b74ef58e3b8a74c
League Student <student@jointheleague.org>
2025-10-07 15:36:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -20,7 +20,7 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # Your code here
 def shape(sides=3):
     tina.begin_fill()
-    tina.fillcolor(258)
+    tina.fillcolor(random.randint(0, 255))
     tina.circle(50, steps= sides)
     tina.end_fill()
     tina.penup()
